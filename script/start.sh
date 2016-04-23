a2d3d7449ad3036290138432578ec287a5fc9691
Sergey Abramchuk <ss.abramchuk@gmail.com>
2016-04-23 21:06:43 +0300

    Got rid of environment variables and used network aliases

--- script/start.sh
+++ script/start.sh
@@ -9,33 +9,31 @@ j2 /home/app/taiga/conf-template/frontend.conf.j2 > /home/app/taiga/front-end/di
 # Configure ssl
 if [ "$TAIGA_SSL_ENABLE" = True -a ! -z "${TAIGA_SSL_KEY}" -a ! -z "${TAIGA_SSL_CERT}" ]
 then
-echo "${TAIGA_SSL_CERT}" > /etc/nginx/certs/$TAIGA_DOMAIN.crt
-echo "${TAIGA_SSL_KEY}" > /etc/nginx/certs/$TAIGA_DOMAIN.key
+    echo "${TAIGA_SSL_CERT}" > /etc/nginx/certs/$TAIGA_DOMAIN.crt
+    echo "${TAIGA_SSL_KEY}" > /etc/nginx/certs/$TAIGA_DOMAIN.key
 fi
 
 # Configure PostgreSQL DB for Taiga
 echo "Waiting for Postgresql to be available..."
-while ! nc -z $DBSERVER_PORT_5432_TCP_ADDR $DBSERVER_PORT_5432_TCP_PORT; do
-  sleep 1
-done
+wait-for-it pgsql-server:5432 -t 60
 
 POPULATE=false
 
-echo "Check if taiga user exists"
-USEREXIST=$(PGPASSWORD=$DBSERVER_ENV_POSTGRES_PASSWORD psql -h "$DBSERVER_PORT_5432_TCP_ADDR" -p "$DBSERVER_PORT_5432_TCP_PORT" -U "$DBSERVER_ENV_POSTGRES_USER" -tAc "SELECT rolname FROM pg_roles WHERE rolname='$TAIGA_POSTGRES_USER'" | wc -l)
+echo "Check whether taiga user exists"
+USEREXIST=$(PGPASSWORD=$POSTGRES_PASSWORD psql -h pgsql-server -p 5432 -U "$POSTGRES_USER" -tAc "SELECT rolname FROM pg_roles WHERE rolname='$TAIGA_POSTGRES_USER'" | wc -l)
 if [ $USEREXIST -eq 0 ]
 then
     echo "User doesn't exist. Creating new one."
-    PGPASSWORD=$DBSERVER_ENV_POSTGRES_PASSWORD psql -h "$DBSERVER_PORT_5432_TCP_ADDR" -p "$DBSERVER_PORT_5432_TCP_PORT" -U "$DBSERVER_ENV_POSTGRES_USER" -c "CREATE USER $TAIGA_POSTGRES_USER WITH PASSWORD '$TAIGA_POSTGRES_PASSWORD'"
+    PGPASSWORD=$POSTGRES_PASSWORD psql -h pgsql-server -p 5432 -U "$POSTGRES_USER" -c "CREATE USER $TAIGA_POSTGRES_USER WITH PASSWORD '$TAIGA_POSTGRES_PASSWORD'"
     POPULATE=true
 fi
 
-echo "Check if taiga database exists"
-DBEXIST=$(PGPASSWORD=$DBSERVER_ENV_POSTGRES_PASSWORD psql -lqt -h "$DBSERVER_PORT_5432_TCP_ADDR" -p "$DBSERVER_PORT_5432_TCP_PORT" -U "$DBSERVER_ENV_POSTGRES_USER" | cut -d \| -f 1 | grep -w "$TAIGA_POSTGRES_DB" | wc -l)
+echo "Check whether taiga database exists"
+DBEXIST=$(PGPASSWORD=$POSTGRES_PASSWORD psql -lqt -h pgsql-server -p 5432 -U "$POSTGRES_USER" | cut -d \| -f 1 | grep -w "$TAIGA_POSTGRES_DB" | wc -l)
 if [ $DBEXIST -eq 0 ]
 then
     echo "Database doesn't exist. Creating new one."
-    PGPASSWORD=$DBSERVER_ENV_POSTGRES_PASSWORD psql -h "$DBSERVER_PORT_5432_TCP_ADDR" -p "$DBSERVER_PORT_5432_TCP_PORT" -U "$DBSERVER_ENV_POSTGRES_USER" -c "CREATE DATABASE $TAIGA_POSTGRES_DB OWNER $TAIGA_POSTGRES_USER"
+    PGPASSWORD=$POSTGRES_PASSWORD psql -h pgsql-server -p 5432 -U "$POSTGRES_USER" -c "CREATE DATABASE $TAIGA_POSTGRES_DB OWNER $TAIGA_POSTGRES_USER"
     POPULATE=true
 fi
 
@@ -59,33 +57,32 @@ cd /
 if [ "$TAIGA_EVENTS_ENABLE" = True ]
 then
     echo "Waiting for RabbitMQ to be available..."
-    while ! nc -z $EVENTSERVER_PORT_15672_TCP_ADDR $EVENTSERVER_PORT_15672_TCP_PORT; do
-      sleep 1
-    done
+    wait-for-it rabbitmq-server:5672 -t 60
 
-    RABBITMQ_ADMIN_INSTALLED=$(which rabbitmqadmin | wc -l)
-    if [ $RABBITMQ_ADMIN_INSTALLED -eq 0 ]
+    RABBIT_ADMIN_INSTALLED=$(which rabbitmqadmin | wc -l)
+    if [ $RABBIT_ADMIN_INSTALLED -eq 0 ]
     then
         echo "Util rabbitmqadmin wasn't found. Downloading..."
-        curl http://$EVENTSERVER_PORT_15672_TCP_ADDR:$EVENTSERVER_PORT_15672_TCP_PORT/cli/rabbitmqadmin -o /usr/bin/rabbitmqadmin
-        chmod +x /usr/bin/rabbitmqadmin
+        wait-for-it rabbitmq-server:15672
+        curl http://rabbit-server:15672/cli/rabbitmqadmin -o /usr/local/bin/rabbitmqadmin
+        chmod +x /usr/local/bin/rabbitmqadmin
     fi
 
-    echo "Check if taiga user exists"
-    USEREXIST=$(rabbitmqadmin -H $EVENTSERVER_PORT_5672_TCP_ADDR -u $EVENTSERVER_ENV_RABBITMQ_DEFAULT_USER -p $EVENTSERVER_ENV_RABBITMQ_DEFAULT_PASS list users | grep $TAIGA_RABBITMQ_USER | wc -l)
+    echo "Check whether taiga user exists"
+    USEREXIST=$(rabbitmqadmin -H rabbitmq-server -u $RABBITMQ_DEFAULT_USER -p $RABBITMQ_DEFAULT_PASS list users | grep $TAIGA_RABBITMQ_USER | wc -l)
     if [ $USEREXIST -eq 0 ]
     then
         echo "User doesn't exist. Creating new one."
-        rabbitmqadmin -H $EVENTSERVER_PORT_5672_TCP_ADDR -u $EVENTSERVER_ENV_RABBITMQ_DEFAULT_USER -p $EVENTSERVER_ENV_RABBITMQ_DEFAULT_PASS declare user name=$TAIGA_RABBITMQ_USER password=$TAIGA_RABBITMQ_PASSWORD tags=administrator
+        rabbitmqadmin -H rabbitmq-server -u $RABBITMQ_DEFAULT_USER -p $RABBITMQ_DEFAULT_PASS declare user name=$TAIGA_RABBITMQ_USER password=$TAIGA_RABBITMQ_PASSWORD tags=administrator
     fi
 
-    echo "Check if taiga vhost exists"
-    VHOSTEXIST=$(rabbitmqadmin -H $EVENTSERVER_PORT_5672_TCP_ADDR -u $EVENTSERVER_ENV_RABBITMQ_DEFAULT_USER -p $EVENTSERVER_ENV_RABBITMQ_DEFAULT_PASS list vhosts | grep $TAIGA_RABBITMQ_VHOST | wc -l)
+    echo "Check whether taiga vhost exists"
+    VHOSTEXIST=$(rabbitmqadmin -H rabbitmq-server -u $RABBITMQ_DEFAULT_USER -p $RABBITMQ_DEFAULT_PASS list vhosts | grep $TAIGA_RABBITMQ_VHOST | wc -l)
     if [ $VHOSTEXIST -eq 0 ]
     then
         echo "Vhost doesn't exist. Creating new one."
-        rabbitmqadmin -H $EVENTSERVER_PORT_5672_TCP_ADDR -u $EVENTSERVER_ENV_RABBITMQ_DEFAULT_USER -p $EVENTSERVER_ENV_RABBITMQ_DEFAULT_PASS declare vhost name=$TAIGA_RABBITMQ_VHOST
-        rabbitmqadmin -H $EVENTSERVER_PORT_5672_TCP_ADDR -u $EVENTSERVER_ENV_RABBITMQ_DEFAULT_USER -p $EVENTSERVER_ENV_RABBITMQ_DEFAULT_PASS declare permission vhost=$TAIGA_RABBITMQ_VHOST user=$TAIGA_RABBITMQ_USER configure='.*' write='.*' read='.*'
+        rabbitmqadmin -H rabbitmq-server -u $RABBITMQ_DEFAULT_USER -p $RABBITMQ_DEFAULT_PASS declare vhost name=$TAIGA_RABBITMQ_VHOST
+        rabbitmqadmin -H rabbitmq-server -u $RABBITMQ_DEFAULT_USER -p $RABBITMQ_DEFAULT_PASS declare permission vhost=$TAIGA_RABBITMQ_VHOST user=$TAIGA_RABBITMQ_USER configure='.*' write='.*' read='.*'
     fi
 fi
 
