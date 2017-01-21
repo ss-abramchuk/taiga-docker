d4d273fa969afb66a9e8b7e671e8ce5ade833d3c
Sergey Abramchuk <ss.abramchuk@gmail.com>
2017-01-21 20:16:24 +0300

    Fix malfunction of backup process

--- taiga-backend/scripts/backup.sh
+++ taiga-backend/scripts/backup.sh
@@ -4,9 +4,8 @@ set -e
 
 if [ -n "$TAIGA_BACKUP_STORAGE" ]
 then
-    BACKUP_LOG_FILE=/var/log/backup.log
-    echo "[$(date -R)] Backing up taiga media folder" >> $BACKUP_LOG_FILE 2>&1
-    python /home/app/taiga/backend/manage.py mediabackup --compress --clean >> $BACKUP_LOG_FILE 2>&1
-    echo "[$(date -R)] Backing up taiga database" >> $BACKUP_LOG_FILE 2>&1
-    python /home/app/taiga/backend/manage.py dbbackup --compress --clean >> $BACKUP_LOG_FILE 2>&1
+    echo "[$(date -R)] Backing up taiga media folder"
+    python3 /home/app/taiga/backend/manage.py mediabackup --compress --clean
+    echo "[$(date -R)] Backing up taiga database"
+    python3 /home/app/taiga/backend/manage.py dbbackup --compress --clean
 fi
