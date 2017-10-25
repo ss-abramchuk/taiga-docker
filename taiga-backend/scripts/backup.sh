ec78f1febfb534c16e5a88b94e07494440e1d86c
Sergey Abramchuk <dev@ss-abramchuk.me>
2017-10-25 23:13:58 +0300

    Update backup commands

--- taiga-backend/scripts/backup.sh
+++ taiga-backend/scripts/backup.sh
@@ -5,7 +5,7 @@ set -e
 if [ -n "$TAIGA_BACKUP_STORAGE" ]
 then
     echo "[$(date -R)] Backing up taiga media folder"
-    python3 /home/app/taiga/backend/manage.py mediabackup --compress --clean
+    python3 /home/app/taiga/backend/manage.py mediabackup -z -c
     echo "[$(date -R)] Backing up taiga database"
-    python3 /home/app/taiga/backend/manage.py dbbackup --compress --clean
+    python3 /home/app/taiga/backend/manage.py dbbackup -c
 fi
